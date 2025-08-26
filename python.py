d86c636e1074f37b1c9b0905d45f197a94ba012f
joselu-del <joselucas.araujo13@gmail.com>
2025-08-25 21:17:10 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+numero = float(input("Digite um numero de 0 a 10:"))
+
+if numero == 1:
+    print("Bom dia!")
+    
+elif numero == 2:
+    print("Boa tarde!")
+    
+elif numero == 3:
+    print("Boa noite!")
+    
+else:
+    print("Voce digitou o numero errado")
+
+    
---
+++ python_lucas.py
@@ -0,0 +1,12 @@
+nota = float(input("Digite a nota do aluno (0 a 10):"))
+
+if nota >= 6.0:
+    print("Aluno aprovado! Parabéns!")
+    
+if nota == 0.0:
+    print("Aluno reprovado de ano.")
+    
+else:
+    print("aluno recuperação.")
+    
+    
